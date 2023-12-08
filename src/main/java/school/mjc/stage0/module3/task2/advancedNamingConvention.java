5c78600503f98308ef9878a35c13ff3856d977dd
Martynas <martynas.bartnykas@gmail.com>
2023-12-08 20:53:29 +0200

    Made same modifications regarding task.

--- src/main/java/school/mjc/stage0/module3/task2/advancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/advancedNamingConvention.java
@@ -1,16 +1,40 @@
 package school.mjc.stage0.module3.task2;
 
-public class advancedNamingConvention {
+public record advancedNamingConvention(int age, int phoneNumber) {
     private static final int adult_age = 18;
 
-    private int age;
-    private int phoneNumber;
+    // Constructor to initialize age and phoneNumber
 
-    void callToFriend() {
-
+    // Method to check if a person is an adult
+    public boolean isAdult() {
+        return age >= adult_age;
     }
 
-    void callByNumber(int number) {
+    void callToFriend() {
+        System.out.println("Calling a friend...");
+    }
 
+    void callByNumber() {
+        int number = 987654321;
+        System.out.println("Calling number: " + number);
+    }
+
+
+    public static void main(String[] args) {
+        // Creating an instance of the class
+        advancedNamingConvention person = new advancedNamingConvention(25, 123456789);
+
+        // Using the getter methods
+        System.out.println("Age: " + person.age());
+        System.out.println("Phone Number: " + person.phoneNumber());
+
+        // Using the isAdult method
+        System.out.println("Is Adult: " + person.isAdult());
+
+        // Using the callToFriend method
+        person.callToFriend();
+
+        // Using the callByNumber method
+        person.callByNumber();
     }
 }
