6063c3fec8e531ab36da913ba5ec917fc4aa161c
Martynas <martynas.bartnykas@gmail.com>
2023-12-08 20:31:07 +0200

    Made same modifications regarding task.

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -2,14 +2,25 @@ package school.mjc.stage0.module3.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int _variable = 10;
-        int variable_One = 1;
-        int variableTwo = 2;
+        int variable_zero = 10;
+        int variable_one = 1;
+        int variable_two = 2;
         int variable_three = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEfIVE = 5;
-        int VariableSix = 6;
-        int variableseven = 7;
-        int VariableEight = 8;
+        final int variableFour = 4;
+        final int variableFive = 5;
+        int variableSix = 6;
+        int variableSeven = 7;
+        int variableEight = 8;
+
+        // Print the variables
+        System.out.println("variable_zero: " + variable_zero);
+        System.out.println("variable_one: " + variable_one);
+        System.out.println("variable_two: " + variable_two);
+        System.out.println("variable_three: " + variable_three);
+        System.out.println("variableFour: " + variableFour);
+        System.out.println("variableFive: " + variableFive);
+        System.out.println("variableSix: " + variableSix);
+        System.out.println("variableSeven: " + variableSeven);
+        System.out.println("variableEight: " + variableEight);
     }
 }
